1747b58823b74ce4d6b3a713cabda62d83e0737b
Vamsi <vamsilver04@bk.ru>
2023-12-19 20:11:09 +0300

    Add project files.

---
+++ OptoVIP/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using OptoVIP.ADO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OptoVIP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static OptoVIPEntities Connection = new OptoVIPEntities();
+        public static ADO.User CurrentUser = new ADO.User();
+    }
+}
